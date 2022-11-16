02902c6b51b4e269eef082c5c68b7ed3218c845c
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2022-11-16 16:30:04 +0900

    221116/정선아/시간관련구현
    1. 실제 시간대에 맞게 기상/취침시간 출력
    2. 정해진 기상/취침시간에 따라 남은 시간 타이머 구현

--- 2022SemesterProject_Ghost/Assets/Script/Manager/GameManager.cs
+++ 2022SemesterProject_Ghost/Assets/Script/Manager/GameManager.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
     public JsonManager jsonManager;
 
-    [HideInInspector]
-    //시간 변수
+    public Text counterText, wakeupDateText, wakeupTimeText, wakeupText;
+    [HideInInspector] public TimeSpan spare;
+    [HideInInspector] public DateTime midnightTime, standardTime, now;
 
     public static GameManager Instance
     {
@@ -18,7 +21,7 @@ public class GameManager : MonoBehaviour
             {
                 return null;
             }
-            return instance;
+            return instance;    
         }
     }
 
@@ -29,14 +32,46 @@ public class GameManager : MonoBehaviour
             instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        else
+        /*else
         {
             Destroy(gameObject);
-        }
+        }*/
     }
 
     private void Start()
     {
         jsonManager = new JsonManager();
     }
+
+    void Update()
+    {
+       Time(); 
+    }
+
+    void Time(){
+        string standard = DateTime.Now.ToString("yyyy/MM/dd") + " 08:00"; // 오늘 오전 8시
+        standardTime = Convert.ToDateTime(standard); //기준시간을 오늘날짜 오전 8시로 설정
+        now=DateTime.Now; //현재시간
+
+        int result=DateTime.Compare(DateTime.Now, standardTime);
+        if(result>=0){  // 오전 8시 이후인 경우(영혼 기상 후)
+            wakeupText.text="취침 시간";
+            wakeupTimeText.text="00시"; //자정          
+            wakeupDateText.text=now.AddDays(1).ToString("yyyy년 MM월 dd일"); // 기상날짜
+            
+            string midnight=now.AddDays(1).ToString("yyyy/MM/dd")+" 00:00"; // 자정
+            midnightTime=Convert.ToDateTime(midnight);
+            spare=midnightTime-now; //자정-현재시간
+            counterText.text="<color=#DC143C>" + spare.ToString(@"hh\:mm\:ss") + "</color>";
+        }
+        else{ // 오전 8시 이전(영혼 취침 중)
+            wakeupText.text="기상 시간";
+            wakeupTimeText.text="08시";
+
+            spare=standardTime-now;
+            wakeupDateText.text=standardTime.ToString("yyyy년 MM월 dd일");
+            counterText.text="<color=#32CD32>" + spare.ToString(@"hh\:mm\:ss") + "</color>";
+        }
+    }
+
 }
